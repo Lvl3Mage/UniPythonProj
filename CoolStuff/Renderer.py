b1992b2ed515ebfdcf34e50ee3937734d7c141a6
Lvl3Mage <lvlthreemage@gmail.com>
2022-12-15 06:37:46 +0100

    Added perspective to Renderer.py

--- CoolStuff/Renderer.py
+++ CoolStuff/Renderer.py
@@ -15,30 +15,42 @@ tracer(0, 0)
 trt.hideturtle()
 canvas = getcanvas()
 
+scale = 100
 verts = [
-	Vector3(1, -2, 2)*100, Vector3(1,2,1)*100, Vector3(1,-1,-1)*100,
-	Vector3(-1, -2, 2)*100, Vector3(-1,2,1)*100, Vector3(-1,-1,-1)*100,
+	Vector3(1, -1, -1)*scale, Vector3(1,-1,1)*scale, Vector3(-1,-1,1)*scale, Vector3(-1, -1, -1)*scale,
+	Vector3(1, 1, -1)*scale, Vector3(1,1,1)*scale, Vector3(-1,1,1)*scale, Vector3(-1, 1, -1)*scale,
 ]
-tris = [(0,1,2),(3,4,5),(0,1,4),(4,5,2)]
+tris = [
+	(0,1,2),(2,3,0),
 
+	(0,1,4),(4,5,1),
+	(1,2,5),(5,6,2),
+	(2,3,6),(6,7,3),
+	(3,0,7),(7,4,0),
+
+	(4,5,6),(6,7,4),
+]
+zOffset = 250
+FOV = 100
 trt.fillcolor(0.4, 0.4, 0.4)
 while trt:
 	trt.clear()
 	
 	for tri in tris:
 		trt.penup()
-		trt.goto(verts[tri[0]].x, verts[tri[0]].y)
-		trt.begin_fill()
+		triVerts = GetTriangle(tri)
+		trt.goto(triVerts[0].x*FOV/(triVerts[0].z+zOffset), triVerts[0].y*FOV/(triVerts[0].z+zOffset))
+		# trt.begin_fill()
 		trt.pendown()
-		trt.goto(verts[tri[1]].x, verts[tri[1]].y)
-		trt.goto(verts[tri[2]].x, verts[tri[2]].y)
-		trt.goto(verts[tri[0]].x, verts[tri[0]].y)
-		trt.end_fill()			
+		trt.goto(triVerts[1].x*FOV/(triVerts[1].z+zOffset), triVerts[1].y*FOV/(triVerts[1].z+zOffset))
+		trt.goto(triVerts[2].x*FOV/(triVerts[2].z+zOffset), triVerts[2].y*FOV/(triVerts[2].z+zOffset))
+		trt.goto(triVerts[0].x*FOV/(triVerts[0].z+zOffset), triVerts[0].y*FOV/(triVerts[0].z+zOffset))
+		# trt.end_fill()			
 
 	for i in range(len(verts)):
 		vert = verts[i]
 		planeVert = Vector2(vert.x, vert.z)
-		planeVert.RotateByAngle(0.2)
+		planeVert.RotateByAngle(0.05)
 		verts[i] = Vector3(planeVert.x, vert.y, planeVert.y)
 		print(verts[i])
 	update()
